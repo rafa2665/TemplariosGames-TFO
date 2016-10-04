24c30187166ec20eb5bcada0158f577609d473aa
Rafael Silva Alves <rafa266528@gmail.com>
2016-10-04 19:42:45 -0300

    Add files via upload

---
+++ SlimeMorte.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeMorte : MonoBehaviour {
+    [SerializeField]
+    GameObject slime;
+
+    void OnTriggerEnter(Collider outro)
+    {
+        GameObject novoSlime = slime;
+        Instantiate(novoSlime, transform.position, Quaternion.identity);
+        novoSlime.GetComponent<Transform>().localScale = (transform.localScale / 2);
+        Destroy(gameObject, 0.001f);
+    }
+}
---
+++ TrocarVisao.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrocarVisao : MonoBehaviour {
+    Vector3 posicaoT, posicaoP;
+    [SerializeField]
+    Transform cam;
+	// Use this for initialization
+	void Start () {
+        posicaoP = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        posicaoT = cam.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ApertouMouse();
+	}
+
+    void ApertouMouse()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (cam.transform.position == posicaoT)
+                cam.transform.position = posicaoP;
+        }
+
+        else if (Input.GetMouseButtonUp(1))
+        {
+            if (cam.transform.position == posicaoP)
+                cam.transform.position = posicaoT;
+        }
+    }
+}
