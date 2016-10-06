e7b59f4989e8669d87656d63783738f4269f7040
Rafael Silva Alves <rafa266528@gmail.com>
2016-10-06 19:01:15 -0300

    Update TrocarVisao.cs

--- TrocarVisao.cs
+++ TrocarVisao.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TrocarVisao : MonoBehaviour {
+public class TrocarVisao : MonoBehaviour {//esse script vai no player
     Vector3 posicaoT, posicaoP;
     [SerializeField]
     Transform cam;
 	// Use this for initialization
 	void Start () {
-        posicaoP = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+        posicaoP = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);//defina a posicao da primeira pessoa
         posicaoT = cam.transform.position;
 	}
 	
