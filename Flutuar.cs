e65399c4b2f76fcb4a5802ed6507587a57d3cbef
Rafael Silva Alves <rafa266528@gmail.com>
2016-11-17 20:29:01 -0200

    Update Flutuar.cs

--- Flutuar.cs
+++ Flutuar.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class Flutuar : MonoBehaviour {
+public class Flutuar : MonoBehaviour
+{
     float posicaoY;
     enum posicaoAtual { emBaixo, emCima };
     posicaoAtual posicao = posicaoAtual.emBaixo;
-
+    float forca;
     void Start()
     {
         posicaoY = transform.position.y;
         transform.position = new Vector3(transform.position.x, posicaoY - 1.1f, transform.position.z);
+        forca = 0;
     }
 
     void Update()
@@ -20,20 +22,20 @@ public class Flutuar : MonoBehaviour {
     void Flutuando()
     {
         if (transform.position.y < posicaoY - 1)
-            posicao = posicaoAtual.emBaixo;
+          posicao = posicaoAtual.emBaixo;
         else if (transform.position.y > posicaoY + 1)
-            posicao = posicaoAtual.emCima;
-        float forca = 0;
+          posicao = posicaoAtual.emCima;
+        Debug.Log(forca);
         switch (posicao)
-        {
-            case posicaoAtual.emBaixo:
-                forca = Mathf.Lerp(forca, posicaoY + 1, Time.deltaTime);//mude de lerp para movetoward
-                transform.Translate(0, forca, 0);
-                break;
-            case posicaoAtual.emCima:
-                forca = Mathf.Lerp(forca, posicaoY - 1, Time.deltaTime);//mude de lerp para movetoward
-                transform.Translate(0, forca, 0);
-                break;
-        }
+           {
+               case posicaoAtual.emBaixo:
+                   forca = Mathf.Lerp(forca, posicaoY + 1, 0.01f);
+                   transform.Translate(0, forca, 0);
+                   break;
+               case posicaoAtual.emCima:
+                   forca = Mathf.Lerp(forca, posicaoY - 1, 0.01f);
+                   transform.Translate(0, forca, 0);
+                   break;
+           }
     }
 }
